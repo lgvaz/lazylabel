39ee7dd95d439ef7b08ed178cedad07552f93975
lgvaz <lucasgouvaz@gmail.com>
2020-04-08 23:59:14 -0300

    first prototype for Pipeline2

--- pigboat/_nbdev.py
+++ pigboat/_nbdev.py
@@ -2,9 +2,14 @@
 
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
-index = {}
+index = {"TypeDispatch2": "00_core.ipynb",
+         "DispatchReg2": "00_core.ipynb",
+         "typedispatch2": "00_core.ipynb",
+         "reduce_lbls": "00_core.ipynb",
+         "compose_tfms2": "00_core.ipynb",
+         "Pipeline2": "00_core.ipynb"}
 
-modules = []
+modules = ["core.py"]
 
 doc_url = "https://lgvaz.github.io/pigboat/"
 
